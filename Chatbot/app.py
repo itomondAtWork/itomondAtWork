b6b905417fc2b3d7e5e36f71c78098c942232441
itomondAtWork <ryota.ito@sap.com>
2024-05-27 14:07:17 +0900

    Update

--- Chatbot/app.py
+++ Chatbot/app.py
@@ -1,13 +1,10 @@
 import os
-import tempfile
+import requests
 import streamlit as st
 from hdbcli import dbapi
-from streamlit_chat import message
-from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from gen_ai_hub.proxy.core.proxy_clients import get_proxy_client
+from gen_ai_hub.proxy.langchain.openai import ChatOpenAI, OpenAIEmbeddings
 from langchain.chains import ConversationalRetrievalChain
-from langchain.memory import ConversationBufferMemory
-from langchain_community.document_loaders import PyPDFLoader, WebBaseLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores.hanavector import HanaDB
 from langchain_core.prompts import PromptTemplate
 from dotenv import load_dotenv
@@ -20,8 +17,8 @@ def set_custom_styles():
             border-radius: 20px;
             font-size: 25px;
             padding: 10px 24px;
-            background-color: #FF4B4B; /* ボタンの背景色 */
-            color: white; /* ボタンの文字色 */
+            background-color: #FF4B4B;
+            color: white;
         }
         .stTextInput>div>div>input {
             font-size: 16px;
@@ -31,15 +28,14 @@ def set_custom_styles():
         .css-1d391kg {
             padding: 10px 20px 20px;
         }
-        /* チャットメッセージ用のカスタムスタイル */
         .message-user {
-            background-color: #D0F0C0; /* ユーザーメッセージの背景色 */
+            background-color: #D0F0C0;
             padding: 10px;
             border-radius: 15px;
             margin: 5px;
         }
         .message-bot {
-            background-color: #F0D0FF; /* ボットメッセージの背景色 */
+            background-color: #F0D0FF;
             padding: 10px;
             border-radius: 15px;
             margin: 5px;
@@ -51,47 +47,34 @@ def set_custom_styles():
 def main():
     set_custom_styles()
     load_dotenv()
-
-    user_api_key = os.getenv('OPENAI_API_KEY')
-    os.environ['OPENAI_API_KEY'] = user_api_key
-
-    connection, db = initialize_database()
-
-    uploaded_files = st.sidebar.file_uploader("Upload PDFs", type="pdf", accept_multiple_files=True)
-    webpage_urls = st.sidebar.text_area("Enter webpage URLs to load content", placeholder="https://example.com\nhttps://example2.com")
-    url_list = st.session_state.get('url_list', [])
-
-    if st.sidebar.button('Add URL'):
-        if webpage_urls:
-            url_list.extend(webpage_urls.split())
-            st.session_state['url_list'] = url_list
-            webpage_urls = ""  # Clear the text area
-
-    url_container = st.sidebar.container()
-    for i, url in enumerate(url_list):
-        col1, col2 = url_container.columns([0.8, 0.2])
-        col1.write(url)
-        if col2.button("Remove", key=f'remove-{i}'):
-            url_list.remove(url)
-            st.session_state['url_list'] = url_list
-
-    if uploaded_files:
-        process_pdfs(uploaded_files, db)
-    if url_list:
-        for url in url_list:
-            process_webpage(url, db)
-
-    chain = setup_chain(db)
+    token = get_token()
+    proxy_client = get_proxy_client('gen-ai-hub', token=token)
+    db = connect_database(proxy_client)
+    chain = setup_chain(db, proxy_client)
     initialize_session_state()
     display_interface(chain)
 
-def process_webpage(url, db):
-    loader = WebBaseLoader(url)
-    loader.requests_kwargs = {'verify': False}
-    text = loader.load()
-    db.add_documents(text)
+def get_token():
+    auth_url = os.getenv('AICORE_AUTH_URL')
+    client_id = os.getenv('AICORE_CLIENT_ID')
+    client_secret = os.getenv('AICORE_CLIENT_SECRET')
 
-def initialize_database():
+    token_url = f"{auth_url}/oauth/token"
+    data = {
+        'grant_type': 'client_credentials',
+        'client_id': client_id,
+        'client_secret': client_secret
+    }
+
+    response = requests.post(token_url, data=data)
+    response.raise_for_status()
+
+    token = response.json().get('access_token')
+    if not token:
+        raise ValueError("トークンが取得できませんでした。")
+    return token
+
+def connect_database(proxy_client):
     connection = dbapi.connect(
         address=os.getenv("HANA_DB_ADDRESS"),
         port=os.getenv("HANA_DB_PORT"),
@@ -100,30 +83,16 @@ def initialize_database():
         autocommit=True,
         sslValidateCertificate=False,
     )
-    embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
+    embeddings = OpenAIEmbeddings(deployment_id=os.getenv('EMBEDDING_DEPLOYMENT_ID'), proxy_client=proxy_client)
     db = HanaDB(
         connection=connection,
         embedding=embeddings,
-        table_name="ITO_DEMO_RETRIEVAL_CHAIN",
+        table_name="FILE_EMBEDDINGS",
     )
-    db.delete(filter={})
-    return connection, db
+    return db
 
-def process_pdfs(uploaded_files, db):
-    for uploaded_file in uploaded_files:
-        with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
-            tmp_file.write(uploaded_file.getvalue())
-            tmp_file_path = tmp_file.name
-        loader = PyPDFLoader(file_path=tmp_file_path)
-        documents = loader.load()
-        db.add_documents(documents)
-
-def setup_chain(db):
-    llm = ChatOpenAI(
-        model="gpt-4-turbo",
-        temperature=0.2,
-        api_key=os.getenv('OPENAI_API_KEY')
-    )
+def setup_chain(db, proxy_client):
+    llm = ChatOpenAI(deployment_id=os.getenv('LLM_DEPLOYMENT_ID'), proxy_client=proxy_client)
     prompt_template = """
     You are a helpful advisor. Context related to the prompt is provided.
     Please answer it by referring to the chat history, but also referring to the following context.
@@ -136,14 +105,10 @@ def setup_chain(db):
     PROMPT = PromptTemplate(
         template=prompt_template, input_variables=["context", "chat_history", "question"]
     )
-    memory = ConversationBufferMemory(
-        memory_key="chat_history", output_key="answer", return_messages=True
-    )
+
     chain = ConversationalRetrievalChain.from_llm(
         llm,
-        db.as_retriever(),
-        return_source_documents=True,
-        memory=memory,
+        retriever=db.as_retriever(),
         verbose=False,
         combine_docs_chain_kwargs={"prompt": PROMPT}
     )
@@ -153,9 +118,9 @@ def initialize_session_state():
     if 'history' not in st.session_state:
         st.session_state['history'] = []
     if 'generated' not in st.session_state:
-        st.session_state['generated'] = ["Hello! Feel free to ask about anything regarding the uploaded PDFs or webpage content."]
+        st.session_state['generated'] = []
     if 'past' not in st.session_state:
-        st.session_state['past'] = ["Hi!"]
+        st.session_state['past'] = []
 
 def display_interface(chain):
     response_container = st.container()
@@ -166,7 +131,7 @@ def display_interface(chain):
             user_input = st.text_input("Input:", placeholder="Please enter your message.", key='input')
             submit_button = st.form_submit_button(label='Send')
         if submit_button and user_input:
-            output = run_conversational_chat(chain, user_input)
+            output = run_conversational_chat(chain, user_input, st.session_state['history'])
             st.session_state['past'].append(user_input)
             st.session_state['generated'].append(output)
     if st.session_state['generated']:
@@ -175,10 +140,16 @@ def display_interface(chain):
                 st.markdown(f'<div class="message-user">😊 {st.session_state["past"][i]}</div>', unsafe_allow_html=True)
                 st.markdown(f'<div class="message-bot">🤖 {st.session_state["generated"][i]}</div>', unsafe_allow_html=True)
 
-def run_conversational_chat(chain, query):
-    result = chain({"question": query, "chat_history": st.session_state['history']})
-    st.session_state['history'].append((query, result["answer"]))
-    return result["answer"]
+def run_conversational_chat(chain, query, chat_history):
+    # チェーンにクエリを投げて結果を取得
+    result = chain({"question": query, "chat_history": chat_history})
+    print("Chain Result:", result)
+
+    # 新しいメッセージをチャット履歴に追加
+    chat_history.append((query, result.get("answer", "No valid response found")))
+
+    # 結果を返す
+    return result.get("answer", "No valid response found")
 
 if __name__ == "__main__":
     main()
